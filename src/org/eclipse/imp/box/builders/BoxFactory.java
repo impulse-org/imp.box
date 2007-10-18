7cf4f258c7946f3abfbe3df2461f2e427b3f73a1
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2007-10-18 15:27:39 +0000

    added some comments to the code added a faster implementation of box2text by using shared binary ATerms instead of the readable textual non-shared representation removed generated LPG Java parser code from repository

--- src/org/eclipse/imp/box/builders/BoxFactory.java
+++ src/org/eclipse/imp/box/builders/BoxFactory.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.util.Calendar;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Platform;
@@ -16,6 +17,10 @@ import org.osgi.framework.Bundle;
 public class BoxFactory {
 	private static String BoxParsetablePath;
 
+	/**
+	 * The external tools called by this class need some files that are
+	 * stored in the plugin bundle. 
+	 */
 	static {
 		Bundle bundle = Platform.getBundle(Activator.kPluginID);
 		URL url = bundle.getResource("Box.tbl");
@@ -26,13 +31,19 @@ public class BoxFactory {
 		}
 	}
 	
-	public static void connect(InputStream in, OutputStream out) throws IOException {
-		byte[] buffer = new byte[8192];
-		int amount;
-		while ((amount = in.read(buffer)) >= 0)
-			out.write(buffer, 0, amount);
-	}
 	
+	/**
+	 * This methods calls external tools to execute the formatting of a box term.
+	 * The term is parsed and then processed to finally result in a formatted text.
+	 * TODO: this implementation may be slow due to the calling of external tools,
+	 * also the tools are required to be on the search path are: "sglr" and "pandora".
+	 * This is obviously only going to work on Un*x platforms like this. 
+	 * 
+	 * @param box
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	public static String box2text(String box) throws IOException, InterruptedException {
 		return formatBox(parseBox(box));
 	}
@@ -67,5 +78,66 @@ public class BoxFactory {
 		}
 		
 		return output;
+	}
+	
+	/** 
+	 * This box2text() implementation is faster for larger terms since
+	 * it makes sure that communication between sglr and pandora is done
+	 * using binary shared ATerms instead of the unshared representation
+	 * used in box2text(). 
+	 * 
+	 * @param boxString
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	
+	public static String fastbox2text(String boxString) throws IOException,
+			InterruptedException {
+		String sglr = "sglr -p " + BoxParsetablePath;
+		Process parser = Runtime.getRuntime().exec(sglr);
+		String pandora = "pandora";
+		Process formatter = Runtime.getRuntime().exec(pandora);
+
+		parser.getOutputStream().write(boxString.getBytes());
+		parser.getOutputStream().close();
+		parser.waitFor();
+
+		pipe(parser.getInputStream(), formatter.getOutputStream());
+		formatter.getOutputStream().close();
+		parser.getOutputStream().close(); 
+		formatter.waitFor();
+
+		String result = StreamUtils.readStreamContents(formatter
+				.getInputStream());
+		formatter.getInputStream().close(); 
+
+		if (parser.exitValue() != 0) {
+			throw new RuntimeException("Box parser failed with exit value:"
+					+ parser.exitValue());
+		}
+
+		if (formatter.exitValue() != 0) {
+			throw new RuntimeException("Box formatter failed with exit value: "
+					+ formatter.exitValue());
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * A helper method that pipes an inputstream to an output stream using
+	 * a small intermediate buffer.
+	 * 
+	 * @param in
+	 * @param out
+	 * @throws IOException
+	 */
+	private static void pipe(InputStream in, OutputStream out) throws IOException {
+		byte[] buffer = new byte[8192];
+		int count;
+		while ((count = in.read(buffer)) >= 0) {
+			out.write(buffer, 0, count);
+		}
 	}
 }
