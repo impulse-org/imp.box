2879b2ca9d6fd9560ed6251c11f64e0c28e7a485
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2007-12-17 20:59:25 +0000

    reverted some changes in the box parse controller and updated to the new interfaces

--- src/org/eclipse/imp/box/parser/BoxParseController.java
+++ src/org/eclipse/imp/box/parser/BoxParseController.java
@@ -2,7 +2,7 @@ package org.eclipse.imp.box.parser;
 
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.imp.language.IAnnotationTypeInfo;
+import org.eclipse.imp.box.parser.Ast.ASTNode;
 import org.eclipse.imp.language.ILanguageSyntaxProperties;
 import org.eclipse.imp.model.ISourceProject;
 import org.eclipse.imp.parser.ILexer;
@@ -13,15 +13,11 @@ import org.eclipse.imp.parser.ISourcePositionLocator;
 import org.eclipse.imp.parser.MessageHandlerAdapter;
 import org.eclipse.imp.parser.SimpleLPGParseController;
 
-public class BoxParseController extends SimpleLPGParseController implements IParseController {
+public class BoxParseController extends SimpleLPGParseController implements
+		IParseController {
 	private BoxParser parser;
 
 	private BoxLexer lexer;
-
-        public BoxParseController() {
-            lexer = new BoxLexer();
-            parser = new BoxParser(lexer.getLexStream());
-        }
 
 	public void initialize(IPath filePath, ISourceProject project, IMessageHandler handler) {
 		super.initialize(filePath, project, handler);
@@ -35,21 +31,27 @@ public class BoxParseController extends SimpleLPGParseController implements IPar
 		return lexer;
 	}
 
-	public ISourcePositionLocator getNodeLocator() {
-		return new BoxASTNodeLocator();
-	} 
 
-        public ILanguageSyntaxProperties getSyntaxProperties() {
-            return null; // TODO provide an implementation
-        }
+	public BoxParseController() {
+	}
 
-        public Object parse(String contents, boolean scanOnly,
+	public Object parse(String contents, boolean scanOnly,
 			IProgressMonitor monitor) {
 		PMMonitor my_monitor = new PMMonitor(monitor);
 		char[] contentsArray = contents.toCharArray();
 
-		lexer.reset(contentsArray, fFilePath.toOSString());
-		parser.reset(lexer.getLexStream());
+		if (lexer == null) {
+			lexer = new BoxLexer(contentsArray, fFilePath.toOSString());
+		} else {
+		    lexer.reset(contentsArray, fFilePath.toOSString());
+		}
+		
+		if (parser == null) {
+			parser = new BoxParser(lexer.getLexStream());
+		} else {
+		    parser.reset(lexer.getLexStream());
+		}
+		
 		parser.getParseStream().setMessageHandler(new MessageHandlerAdapter(handler));
 
 		lexer.lexer(my_monitor, parser.getParseStream()); // Lex the stream to produce the token stream
@@ -57,8 +59,18 @@ public class BoxParseController extends SimpleLPGParseController implements IPar
 			return fCurrentAst; 
 		}
 		
-		fCurrentAst = parser.parser(my_monitor, 0);
+		fCurrentAst = (ASTNode) parser.parser(my_monitor, 0);
 
 		return fCurrentAst;
+	}
+
+	public ISourcePositionLocator getNodeLocator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ILanguageSyntaxProperties getSyntaxProperties() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
