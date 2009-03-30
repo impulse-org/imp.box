50068131018eefa1857cc65ff17b67b20bef9fb5
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-30 17:23:26 +0000

    Pulled implementation of getParser() and getLexer() up to SimpleLPGParseController, now that IParser and ILexer provide sufficient API for most needs.

--- src/org/eclipse/imp/box/parser/BoxParseController.java
+++ src/org/eclipse/imp/box/parser/BoxParseController.java
@@ -13,56 +13,40 @@ package org.eclipse.imp.box.parser;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.imp.box.Activator;
 import org.eclipse.imp.box.parser.Ast.ASTNode;
-import org.eclipse.imp.parser.ILexer;
 import org.eclipse.imp.parser.IParseController;
-import org.eclipse.imp.parser.IParser;
 import org.eclipse.imp.parser.MessageHandlerAdapter;
 import org.eclipse.imp.parser.SimpleLPGParseController;
 import org.eclipse.imp.services.ILanguageSyntaxProperties;
 
 public class BoxParseController extends SimpleLPGParseController implements IParseController {
-	private BoxParser parser;
-
-	private BoxLexer lexer;
-
 	public BoxParseController() {
 	    super(Activator.kLanguageName);
-	}
-
-	@Override
-	public ILexer getLexer() {
-	    return lexer;
-	}
-
-	@Override
-	public IParser getParser() {
-	    return parser;
 	}
 
 	public Object parse(String contents, IProgressMonitor monitor) {
 		PMMonitor my_monitor = new PMMonitor(monitor);
 		char[] contentsArray = contents.toCharArray();
 
-		if (lexer == null) {
-			lexer = new BoxLexer(contentsArray, fFilePath.toOSString());
+		if (fLexer == null) {
+			fLexer = new BoxLexer(contentsArray, fFilePath.toOSString());
 		} else {
-		    lexer.reset(contentsArray, fFilePath.toOSString());
+		    fLexer.reset(contentsArray, fFilePath.toOSString());
 		}
 		
-		if (parser == null) {
-			parser = new BoxParser(lexer.getILexStream());
+		if (fParser == null) {
+			fParser = new BoxParser(fLexer.getILexStream());
 		} else {
-		    parser.reset(lexer.getILexStream());
+		    fParser.reset(fLexer.getILexStream());
 		}
 		
-		parser.getIPrsStream().setMessageHandler(new MessageHandlerAdapter(handler));
+		fParser.getIPrsStream().setMessageHandler(new MessageHandlerAdapter(handler));
 
-		lexer.lexer(my_monitor, parser.getIPrsStream()); // Lex the stream to produce the token stream
+		fLexer.lexer(my_monitor, fParser.getIPrsStream()); // Lex the stream to produce the token stream
 		if (my_monitor.isCancelled()) {
 			return fCurrentAst; 
 		}
 		
-		fCurrentAst = (ASTNode) parser.parser(my_monitor, 0);
+		fCurrentAst = (ASTNode) fParser.parser(my_monitor, 0);
 
 		return fCurrentAst;
 	}
