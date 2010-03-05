975905797a2a797d24a5b1cfee3307a55fa4ef12
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-03-05 17:45:37 +0000

    Got rid of the parse() method, in deference to the base class method implementation.

--- src/org/eclipse/imp/box/parser/BoxParseController.java
+++ src/org/eclipse/imp/box/parser/BoxParseController.java
@@ -10,49 +10,19 @@
 *******************************************************************************/
 package org.eclipse.imp.box.parser;
 
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.imp.box.Activator;
-import org.eclipse.imp.box.parser.Ast.ASTNode;
 import org.eclipse.imp.parser.IParseController;
-import org.eclipse.imp.parser.MessageHandlerAdapter;
 import org.eclipse.imp.parser.SimpleLPGParseController;
 import org.eclipse.imp.services.ILanguageSyntaxProperties;
 
 public class BoxParseController extends SimpleLPGParseController implements IParseController {
 	public BoxParseController() {
 	    super(Activator.kLanguageName);
-	}
-
-	public Object parse(String contents, IProgressMonitor monitor) {
-		PMMonitor my_monitor = new PMMonitor(monitor);
-		char[] contentsArray = contents.toCharArray();
-
-		if (fLexer == null) {
-			fLexer = new BoxLexer(contentsArray, fFilePath.toOSString());
-		} else {
-		    fLexer.reset(contentsArray, fFilePath.toOSString());
-		}
-		
-		if (fParser == null) {
-			fParser = new BoxParser(fLexer.getILexStream());
-		} else {
-		    fParser.reset(fLexer.getILexStream());
-		}
-		
-		fParser.getIPrsStream().setMessageHandler(new MessageHandlerAdapter(handler));
-
-		fLexer.lexer(my_monitor, fParser.getIPrsStream()); // Lex the stream to produce the token stream
-		if (my_monitor.isCancelled()) {
-			return fCurrentAst; 
-		}
-		
-		fCurrentAst = (ASTNode) fParser.parser(my_monitor, 0);
-
-		return fCurrentAst;
+	    fLexer= new BoxLexer();
+	    fParser= new BoxParser();
 	}
 
 	public ILanguageSyntaxProperties getSyntaxProperties() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 }
