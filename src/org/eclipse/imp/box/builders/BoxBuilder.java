013ddeb7f9ea2331c07dc72b113521260ad420af
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-10-29 22:51:31 +0000

    Now uses pure-Java Box interpreter implementation. Echoes the resulting text to a console, for easier debugging.

--- src/org/eclipse/imp/box/builders/BoxBuilder.java
+++ src/org/eclipse/imp/box/builders/BoxBuilder.java
@@ -1,24 +1,26 @@
 /*******************************************************************************
-* Copyright (c) 2008 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Jurgen Vinju (jurgen@vinju.org) - initial API and implementation
-*******************************************************************************/
+ * Copyright (c) 2008 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jurgen Vinju (jurgen@vinju.org) - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.imp.box.builders;
 
-import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.StringBufferInputStream;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.imp.box.Activator;
+import org.eclipse.imp.box.interpreter.BoxInterpreter;
 import org.eclipse.imp.box.parser.BoxParseController;
+import org.eclipse.imp.box.parser.Ast.IBox;
 import org.eclipse.imp.builder.BuilderBase;
 import org.eclipse.imp.builder.BuilderUtils;
 import org.eclipse.imp.builder.MarkerCreator;
@@ -26,135 +28,123 @@ import org.eclipse.imp.language.Language;
 import org.eclipse.imp.language.LanguageRegistry;
 import org.eclipse.imp.model.ISourceProject;
 import org.eclipse.imp.model.ModelFactory;
-import org.eclipse.imp.model.ModelFactory.ModelException;
 import org.eclipse.imp.parser.IParseController;
 import org.eclipse.imp.runtime.PluginBase;
-import org.eclipse.imp.utils.StreamUtils;
+import org.eclipse.ui.console.MessageConsoleStream;
 
-/**
- * @author
- */
 public class BoxBuilder extends BuilderBase {
-	public static final String BUILDER_ID = Activator.kPluginID
-			+ ".builder";
+    public static final String BUILDER_ID= Activator.kPluginID + ".builder";
 
-	public static final String PROBLEM_MARKER_ID = Activator.kPluginID
-			+ ".problem";
+    public static final String PROBLEM_MARKER_ID= Activator.kPluginID + ".problem";
 
-	public static final String LANGUAGE_NAME = "Box";
+    public static final String LANGUAGE_NAME= "Box";
 
-	public static final Language LANGUAGE = LanguageRegistry
-			.findLanguage(LANGUAGE_NAME);
+    public static final Language LANGUAGE= LanguageRegistry.findLanguage(LANGUAGE_NAME);
 
-	protected PluginBase getPlugin() {
-		return Activator.getInstance();
-	}
+    protected PluginBase getPlugin() {
+        return Activator.getInstance();
+    }
 
-	protected String getErrorMarkerID() {
-		return PROBLEM_MARKER_ID;
-	}
+    protected String getErrorMarkerID() {
+        return PROBLEM_MARKER_ID;
+    }
 
-	protected String getWarningMarkerID() {
-		return PROBLEM_MARKER_ID;
-	}
+    protected String getWarningMarkerID() {
+        return PROBLEM_MARKER_ID;
+    }
 
-	protected String getInfoMarkerID() {
-		return PROBLEM_MARKER_ID;
-	}
+    protected String getInfoMarkerID() {
+        return PROBLEM_MARKER_ID;
+    }
 
-	protected boolean isSourceFile(IFile file) {
-		IPath path = file.getRawLocation();
-		if (path == null)
-			return false;
+    protected boolean isSourceFile(IFile file) {
+        IPath path= file.getRawLocation();
+        if (path == null)
+            return false;
 
-		String pathString = path.toString();
-		if (pathString.indexOf("/bin/") != -1)
-			return false;
+        String pathString= path.toString();
+        if (pathString.indexOf("/bin/") != -1)
+            return false;
 
-		return LANGUAGE.hasExtension(path.getFileExtension());
-	}
+        return LANGUAGE.hasExtension(path.getFileExtension());
+    }
 
-	/**
-	 * @return true iff the given file is a source file that this builder should scan
-	 * for dependencies, but not compile as a top-level compilation unit.<br>
-	 * <code>isNonRootSourceFile()</code> and <code>isSourceFile()</code> should never
-	 * return true for the same file.
-	 */
-	protected boolean isNonRootSourceFile(IFile resource) {
-		return false;
-	}
+    /**
+     * @return true iff the given file is a source file that this builder should scan for dependencies, but not compile
+     *         as a top-level compilation unit.<br>
+     *         <code>isNonRootSourceFile()</code> and <code>isSourceFile()</code> should never return true for the same
+     *         file.
+     */
+    protected boolean isNonRootSourceFile(IFile resource) {
+        return false;
+    }
 
-	/**
-	 * Collects compilation-unit dependencies for the given file, and records
-	 * them via calls to <code>fDependency.addDependency()</code>.
-	 */
-	protected void collectDependencies(IFile file) {
-		return;
-	}
+    /**
+     * Collects compilation-unit dependencies for the given file, and records them via calls to
+     * <code>fDependency.addDependency()</code>.
+     */
+    protected void collectDependencies(IFile file) {
+        return;
+    }
 
-	protected boolean isOutputFolder(IResource resource) {
-		return resource.getFullPath().lastSegment().equals("bin");
-	}
+    protected boolean isOutputFolder(IResource resource) {
+        return resource.getFullPath().lastSegment().equals("bin");
+    }
 
-	protected void compile(final IFile file, IProgressMonitor monitor) {
-		try {
-			runParserForCompiler(file, monitor);
-			doRefresh(file.getParent());
-			
-			String absPath = file.getLocation().toOSString();
-			
-			String box = BoxFactory.box2text(StreamUtils.readStreamContents(file.getContents()));
-			FileOutputStream out = new FileOutputStream(absPath + ".fmt");
-			out.write(box.getBytes());
-			out.close();
-	
-			doRefresh(file);
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
+    protected String getConsoleName() {
+        return "Box Console";
+    }
 
-			e.printStackTrace();
-		}
-	}
+    protected void compile(final IFile file, IProgressMonitor monitor) {
+        IBox boxProg= parseAndCreateMarkers(file, monitor);
 
-	protected void runParserForCompiler(final IFile file,
-			IProgressMonitor monitor) {
-		try {
-			// Parse controller is the "compiler" here; parses and reports errors
-			IParseController parseController = new BoxParseController();
+        if (boxProg != null) {
+            try {
+                BoxInterpreter bi= new BoxInterpreter();
+                String result= bi.interpret(boxProg);
 
-			// Marker creator handles error messages from the parse controller (and
-			// uses the parse controller to get additional information about the errors)
-			MarkerCreator markerCreator = new MarkerCreator(file, PROBLEM_MARKER_ID);
+                MessageConsoleStream mcs= getConsoleStream();
+                PrintStream ps= new PrintStream(mcs);
 
-			// If we have a kind of parser that might be receptive, tell it
-			// what types of problem marker the builder will create
-			parseController.getAnnotationTypeInfo().addProblemMarkerType(getErrorMarkerID());
+                mcs.getConsole().clearConsole();
+                ps.println(result);
 
-			// Need to tell the parse controller which file in which project to parse
-			// and also the message handler to which to report errors
-			IProject project = file.getProject();
-			ISourceProject sourceProject = null;
-			try {
-				sourceProject = ModelFactory.open(project);
-			} catch (ModelException me) {
-				System.err
-						.println("BoxParseController.runParserForComplier(..):  Model exception:\n"
-								+ me.getMessage()
-								+ "\nReturning without parsing");
-				return;
-			}
-			parseController.initialize(file.getProjectRelativePath(),
-					sourceProject, markerCreator);
+                IPath filePath= file.getFullPath();
+                IPath outPath= filePath.removeFileExtension().addFileExtension("txt");
+                IFile outFile= file.getWorkspace().getRoot().getFile(outPath);
 
-			String contents = BuilderUtils.getFileContents(file);
+                if (outFile.exists()) {
+                    outFile.setContents(new StringBufferInputStream(result), true, false, monitor);
+                } else {
+                    outFile.create(new StringBufferInputStream(result), true, monitor);
+//                FileOutputStream out= new FileOutputStream(outPath.toOSString());
+//
+//                out.write(result.getBytes());
+//                out.close();
+                }
 
-			// Finally parse it
-			parseController.parse(contents, monitor);
+                doRefresh(file.getParent());
+            } catch (Exception e) {
+                getPlugin().logException("Error while interpreting Box program", e);
+            }
+        }
+    }
 
-			doRefresh(file.getParent());
-		} catch (Exception e) {
-			getPlugin().writeErrorMsg(e.getMessage());
-			e.printStackTrace();
-		}
-	}
+    protected IBox parseAndCreateMarkers(final IFile file, IProgressMonitor monitor) {
+        // Parse the box program using the BoxParseController, and send the parser
+        // messages to a MarkerCreator to produce resource markers.
+        try {
+            IParseController parseController= new BoxParseController();
+            MarkerCreator markerCreator= new MarkerCreator(file, PROBLEM_MARKER_ID);
+            ISourceProject sourceProject= ModelFactory.open(file.getProject());
+            String contents= BuilderUtils.getFileContents(file);
+
+            parseController.initialize(file.getProjectRelativePath(), sourceProject, markerCreator);
+
+            return (IBox) parseController.parse(contents, monitor);
+        } catch (Exception e) {
+            getPlugin().logException("Exception encountered while parsing Box program", e);
+        }
+        return null;
+    }
 }
